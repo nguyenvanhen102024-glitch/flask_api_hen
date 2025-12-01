2677671b6275c0b30ced954796694a5af884b78b
Nguyen Van Hen <nguyenvanhen270182@gmail.com>
2025-12-01 21:44:28 +0700

    first commit

---
+++ app.py
@@ -0,0 +1,56 @@
+from flask import Flask, request, jsonify
+import mysql.connector
+import os
+
+app = Flask(__name__)
+
+def connect_db():
+    return mysql.connector.connect(
+        host=os.getenv("DB_HOST"),
+        user=os.getenv("DB_USER"),
+        password=os.getenv("DB_PASS"),
+        database=os.getenv("DB_NAME"),
+        port=os.getenv("DB_PORT")
+    )
+
+@app.route("/")
+def home():
+    return "API Flask Railway OK!"
+
+@app.route("/getall", methods=["GET"])
+def getall():
+    try:
+        conn = connect_db()
+        cur = conn.cursor(dictionary=True)
+        cur.execute("SELECT * FROM dulieu")
+        rows = cur.fetchall()
+        conn.close()
+        return jsonify({"success": True, "data": rows})
+    except Exception as e:
+        return jsonify({"success": False, "error": str(e)})
+
+@app.route("/add", methods=["POST"])
+def add():
+    try:
+        data = request.get_json()
+        ngay = data.get("ngay")
+        stt = data.get("stt")
+        tenhang = data.get("tenhang")
+        soluong = data.get("soluong")
+        page = data.get("page")
+        pageName = data.get("pageName")
+
+        conn = connect_db()
+        cur = conn.cursor()
+        sql = "INSERT INTO dulieu (ngay, stt, tenhang, soluong, page, pageName) VALUES (%s, %s, %s, %s, %s, %s)"
+        cur.execute(sql, (ngay, stt, tenhang, soluong, page, pageName))
+        # cur.execute("INSERT INTO your_table (name,amount) VALUES (%s,%s)", (name,amount))
+        conn.commit()
+        conn.close()
+
+        return jsonify({"success": True})
+    except Exception as e:
+        return jsonify({"success": False, "error": str(e)})
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8080)
